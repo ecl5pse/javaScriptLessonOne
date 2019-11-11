28167c6da9b5737d241cb9b7af1225ac6f09f088
ecl5pse <fantamas20@gmail.com>
2019-11-11 20:40:47 +0200

    work js

---
+++ assers/js/index.js
@@ -0,0 +1,102 @@
+/*
+let r=100;
+const pi= 3.14;// постояная велечина
+
+/!* x+=y
+ x="other"*!/
+
+ /!* x=x+y;
+ *  x+=y -  краткая записть строки сверху
+ * *!/
+ console.log(2*pi*r);
+*/
+
+ /*
+
+let userValue=prompt("Please input youre number :");
+
+userValue*=userValue;
+
+
+console.log(userValue)
+*/
+
+
+//
+// let firstNumber=prompt("Please input youre number :");
+// firstNumber=parseFloat(firstNumber);
+//
+// const s = firstNumber * firstNumber * firstNumber * firstNumber;
+//
+//
+// console.log(s);
+
+
+// let number = prompt("Please input youre number :");
+//
+// number = parseFloat(number);
+//
+// const cv = 0.6211371;
+//
+// const convect = number*cv;
+//
+// console.log(convect)
+
+
+
+/*
+let numberFirst = prompt("Please input youre number :");
+numberFirst = parseFloat(numberFirst);
+
+let numberSecond = prompt("Please input youre number :");
+numberSecond = parseFloat(numberSecond);
+
+const
+*/
+
+
+
+
+
+
+
+const x = prompt("Please input youre number :");
+
+
+if(isNaN(x)){
+
+
+
+
+
+} else {
+        const result = x % 2; // кратность
+
+    if(result === 0) {
+
+        alert("Number is even");
+
+    } else {
+
+        const result = x % 2; // кратность
+        alert("Number is odd");
+    }
+
+}
+                                                                    //const isNumberEven = result === 0; // строгое равенство
+} else {
+    alert("Value isn`t numbe");
+}
+
+
+console.log("Reset of programm");
+
+
+
+
+
+
+
+
+
+
